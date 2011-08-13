9af236b976659c01e3699827eeb05147c52995f3
Ty Overby <ty@pre-alpha.com>
2011-08-13 00:41:59 -0700

    finished. just missing content

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
-$(document).ready(function() {
-    // For page-load scripts
+$(window).load(function() {
+	if(window.location.hash){
+		jumpto(window.location.hash);	
+	}else{
+		jumpto("#about");
+	}
+
+	$("#nav-list>.btn>a").click(function(){
+		jumpto($(this).attr("href"));
+		
+	});
 });
+
+function jumpto(target){
+	var work=false;	
+
+	//hide previously visible div
+	$(".visible").hide("fast",function(){
+		$(target).show("fast");
+		work=true;	
+	});
+	if(!work){
+		$(target).show("slow");
+	}
+	
+	$(".visible").removeClass("visible");
+	$(target).addClass("visible");
+}
--- js/style.js
+++ js/style.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
-    // For page-load javascript that styles elements
+$(window).load(function() {
+	console.log("testing");
+
+	$("#nav-list>li>a").click(function(){
+		bannerTo($(this).parent().attr('class').split(' ')[1]);	
+	});    
 });
+
+function bannerTo(newClass){
+	$("header").removeClass().addClass(newClass);
+	$("#top-banner").removeClass().addClass(newClass);
+	$("#bottom-banner").removeClass().addClass(newClass);
+}
