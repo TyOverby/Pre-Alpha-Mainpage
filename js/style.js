1ec35f2264c39d4fe8e9f9f44cc27d8565e88c34
TyOverby <ty@pre-alpha.com>
2011-08-29 22:32:27 -0700

    got transitions working

--- js/style.js
+++ js/style.js
@@ -1,13 +1,17 @@
-$(window).load(function() {
-	console.log("testing");
+var numClicks=0;
 
+$(window).load(function() {
 	$("#nav-list>li>a").click(function(){
-		bannerTo($(this).parent().attr('class').split(' ')[1]);	
+		bannerTo($(this).parent().attr('class').split(' ')[1]);
 	});    
+	setTimeout(function(){
+		bannerTo($("a[href="+window.location.hash+"]").parent().attr('class').split(' ')[1]);
+	},100);
 });
 
 function bannerTo(newClass){
-	$("header").removeClass().addClass(newClass);
-	$("#top-banner").removeClass().addClass(newClass);
-	$("#bottom-banner").removeClass().addClass(newClass);
+	console.log("going to "+newClass);
+
+	$("header, #top-banner,#bottom-banner").removeClass();
+	$("header, #top-banner,#bottom-banner").addClass(newClass);
 }
